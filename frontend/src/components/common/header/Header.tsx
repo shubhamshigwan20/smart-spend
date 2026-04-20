35872e5c20da0bca398907e4b28d63f43c9422a2
Shubham Shigwan <shubhamonline024@gmail.com>
2026-04-20 16:35:31 +0530

    added tailwind css to project

--- frontend/src/components/common/header/Header.tsx
+++ frontend/src/components/common/header/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div className="w-20 h-20 border-1 border-blue-500">Header</div>;
 };
 
 export default Header;
