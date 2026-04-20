6c6ad3a1a6968d62efbcf57fc68ed602c0f93f00
Shubham Shigwan <shubhamonline024@gmail.com>
2026-04-20 16:13:09 +0530

    added structure for common components

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import Header from "./common/header/Header";
+import Footer from "./common/footer/Footer";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
--- frontend/src/routes/ProtectedRoutes.tsx
+++ frontend/src/routes/ProtectedRoutes.tsx
@@ -1,9 +1,10 @@
+import Layout from "../components/Layout";
 import { useAuth } from "../context/AuthContext";
-import { Outlet, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoutes = () => {
   const { token } = useAuth();
-  return token ? <Outlet /> : <Navigate to="/" replace />;
+  return token ? <Layout /> : <Navigate to="/" replace />;
 };
 
 export default ProtectedRoutes;
